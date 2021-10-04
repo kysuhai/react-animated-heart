a95bed68a0771206b7715a8c82e0be1cda09e4e5
kysuhai <kysuhai@teneocto.io>
2021-10-04 13:34:33 +0700

    refactor isClick

--- src/components/index.js
+++ src/components/index.js
@@ -3,7 +3,7 @@ import { setup, styled } from "goober";
 
 setup(React.createElement);
 
-const HeartUI = styled('div')(({ isClick, styles }) => [
+const HeartUI = styled('div')(({ isClicked, styles }) => [
 	{
 		width: '100px',
 		height: '100px',
@@ -11,13 +11,13 @@ const HeartUI = styled('div')(({ isClick, styles }) => [
 		cursor: 'pointer',
 		display: 'inline-block'
 	},
-	isClick && {
+	isClicked && {
 		backgroundPosition: '-2799px 2px',
 		transition: 'background 1s steps(28)'
 	},
 	styles
 ]);
 
-export default function Heart({ isClick, onClick, styles }) {
-	return <HeartUI isClick={isClick} onClick={onClick} styles={styles} />;
+export default function Heart({ isClicked, onClick, styles }) {
+	return <HeartUI isClicked={isClicked} onClick={onClick} styles={styles} />;
 }
